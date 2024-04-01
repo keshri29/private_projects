49ac25ee2b1ce5ec0abc57e7397417ab495b7a60
anurag kumar <77048227+keshri29@users.noreply.github.com>
2024-04-01 15:14:39 +0530

    uploaded article-project

---
+++ articleporject/megablog/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function Logo() {
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
