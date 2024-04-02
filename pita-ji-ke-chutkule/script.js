3e287dcc9b117568129cf040e2d3e0a2cd64728e
anurag kumar <77048227+keshri29@users.noreply.github.com>
2024-04-02 21:05:30 +0530

    chrome extension

---
+++ pita-ji-ke-chutkule/script.js
@@ -0,0 +1,8 @@
+fetch('https://hindi-jokes-api.onrender.com/jokes?')
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
+        const jokeElement = document.getElementById('jokeElement');
+
+        jokeElement.innerHTML = jokeText;
+    })
